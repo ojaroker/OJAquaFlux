0a575e7089c680a2c042532d0dc8e9dc3a8bd5ee
Jon Jaroker <git@jaroker.com>
2026-03-07 12:34:18 -0500

    todo: logfile to use filestamp

--- AQUA_Flux_Colgate/setupLogging.ino
+++ AQUA_Flux_Colgate/setupLogging.ino
@@ -1,3 +1,7 @@
+// TODO
+// BUG: log file will overwrite itself
+// Use timestams for filenames instead
+
 // Set the log interval (milliseconds between sensor measurements)
 int LOG_INTERVAL = 10000; // If implementing watchdog (line 184), go to lines 321-328 to manually set the log interval
 RTC_PCF8523 rtc;
