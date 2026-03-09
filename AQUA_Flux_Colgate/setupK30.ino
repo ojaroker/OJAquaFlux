fda3781413c37112b05ce1fbcdd1fb4cf64f201b
Jon Jaroker <git@jaroker.com>
2026-03-09 10:09:27 -0400

    Delay K30 start

--- AQUA_Flux_Colgate/setupK30.ino
+++ AQUA_Flux_Colgate/setupK30.ino
@@ -3,6 +3,8 @@
 // To overcome spurious short-circuit fault when powering K30 with relay,
 // we wait to power K30 until after the Arduino has booted and stabilized.
 
+#define K30_STARTUP_DELAY 3000 // Delay after arduino boot before powering K30 (milliseconds)
+
 void setupK30()
 {
 #if USE_K30
@@ -16,10 +18,10 @@ void setupK30()
 #if DEBUG
     LOG_STREAM.print(F("DEBUG - Turning on K30..."));
 #endif
-
+    delay(K30_STARTUP_DELAY); // Wait for K30 to boot up
     pinMode(K30_RELAY_PIN, OUTPUT);
     digitalWrite(K30_RELAY_PIN, HIGH);
-    delay(3000); // Wait for K30 to boot up
+    delay(2000); // Wait for K30 to boot up
 
 #if DEBUG
     LOG_STREAM.println(F("Done"));
