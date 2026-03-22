3dbfca227a310c2c31f8f52eb4ed73e54b8f88d3
Jon Jaroker <git@jaroker.com>
2026-03-22 19:34:19 -0400

    Add F() to literal strings

--- AQUA_Flux_Colgate/scanI2cBus.ino
+++ AQUA_Flux_Colgate/scanI2cBus.ino
@@ -1,15 +1,22 @@
-void scanI2cBus() {
+void scanI2cBus()
+{
   LOG_STREAM.println(F("Scanning I2C bus for devices..."));
-  for (byte address = 1; address < 127; address++) {
+  for (byte address = 1; address < 127; address++)
+  {
     Wire.beginTransmission(address);
     byte error = Wire.endTransmission();
-    if (error == 0) {
+    if (error == 0)
+    {
       LOG_STREAM.print(F("I2C device found at address 0x"));
-      if (address < 16) LOG_STREAM.print("0");
+      if (address < 16)
+        LOG_STREAM.print(F("0"));
       LOG_STREAM.println(address, HEX);
-    } else if (error == 4) {
+    }
+    else if (error == 4)
+    {
       LOG_STREAM.print(F("Unknown error at address 0x"));
-      if (address < 16) LOG_STREAM.print("0");
+      if (address < 16)
+        LOG_STREAM.print(F("0"));
       LOG_STREAM.println(address, HEX);
     }
   }
